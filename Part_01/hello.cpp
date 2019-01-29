d52abc7030a89c350f612a28cd495226e52f88c3
Ezio <assassinste@gmail.com>
2019-01-30 00:18:54 +0800

    Chapter 1~2: nearly finished

---
+++ Part_01/hello.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <iomanip>
+
+namespace Boilerplate
+{
+/*
+    Code within must be called with a prefix.
+    
+    =>  endl	    // using namespace NAMESPACE_NAME;
+    =>  std::endl	// default 
+*/
+}
+
+namespace InitAndAuto
+{
+
+/*
+        All stmts here are valid.
+            int p_init(3.14);
+            int c_init = 3.14;
+            int s_init {3.14};
+
+        The last one is much safer.
+    */
+
+/*
+        unsigned    like 'count students' (pos-num only)
+        signed      by default 
+    */
+
+/*
+        Bi-endian  VS  Little-endian 
+
+        Umm, most of time it doesn't matter (said by author)
+        It does matters when (how u interprete)
+            processing binary data that is from another PC.
+    */
+
+/*
+         `auto` and `{}`
+
+         The `auto` is used to "let compiler deduct its type".
+         Such as 
+             auto m (10)     =>  int 
+             auto n = 20UL	=>  unsigned long 
+
+         Brief example
+             // Yep
+             auto VAR = VAL;
+             auto VAR (VAL);
+
+             // Nope
+             auto VAR {VAL};
+             auto VAR = {VAL};
+    */
+auto w = 10;
+auto h(10);
+auto a{10};    // avoid using this
+auto t = {10}; // it's a list not an int :P
+} // namespace InitAndAuto
+
+namespace IntroCharacter
+{
+
+/*
+        Unicode in C++
+        ->  wchar_t 
+        ->  char16_t
+        ->  char32_t
+    */
+
+// Normal `char` type
+char c1;
+char c2{65};
+char c3{'Y'};
+
+// It's still `char`, but a much much wider range :D
+wchar_t w1{L'Я'};      // prefix with a 'L'
+wchar_t w2{L'\x0438'}; // the 'w' means 'wide'
+
+/*  Ah, I dunno why it doesn't print correctly (None)
+        For the "wider" characters,
+        y'all need to use `wcout` (& `wcin`)
+*/
+// std::wcout << w1 << std::endl;
+// std::wcout << w2 << std::endl;
+
+// => UTF-16, UTF-32
+char16_t cw1{u'Я'};
+char32_t cw2{u'\x044f'};
+} // namespace IntroCharacter
+
+int main()
+{
+    // Compile & run this file by
+    //  clang++ -Wall -std=c++11 YOUR_FILE -o BIN_NAME
+
+    {
+        // But, there ARE taboos
+        //  you should NOT use them if its names like these
+        int i = _CLOCK_UPTIME_RAW;
+        int j = __SIG_ATOMIC_MAX__;
+
+        // #TODO To know why there's `{}`
+        int lucky_number{42};
+
+        // Similarly, there's also a `>>`
+        //  that is used to "reads data from a stream".
+        std::cout << "The '_CLOCK_UPDATE_RAW' equals "
+                  << i
+                  << std::endl;
+
+        // Division
+        std::cout << "Here's the result: "
+                  << 7 / 3 % 2
+                  << " (-> should be '0')"
+                  << ", "
+                  << 10.0 / 4
+                  << std::endl;
+
+        // 0b, 0, 0x, 00'00'01
+        std::cout << "Mixed: "
+                  << 0b11111110 + 0b0000'0001 + 010 + 0x10
+                  << ", "
+                  << 1 + 2U + 3L + 4UL + 5'5'5
+                  << ", "
+                  << 010
+                  << std::endl;
+
+        // Zero init
+        int t1{};
+        float t2{};
+        std::cout << "[initial values]: "
+                  << t1 << ", "
+                  << t2 << ", "
+                  << std::endl;
+
+        // Constant
+        const char aha[]{"aha!"};
+        std::cout << "[const]: "
+                  << aha
+                  << std::endl;
+
+        // initial val & expression
+        int apple_pie{10};
+        int apple_pie_plus{apple_pie + 5};
+
+        // These are the bad examples
+        //  For 'beyond_limit',
+        //      if u use `int`, the compiler will raise errors
+        //  For 'not_the_right_type',
+        //      it'll try to convert the required type.
+        long int beyond_limit{400'0000'0000};
+        int not_the_right_type{'200'};
+        std::cout << "[bad examples]: "
+                  << beyond_limit << ", "
+                  << not_the_right_type << ", "
+                  << std::endl;
+    }
+
+    {
+        // Properties of the fundamental types
+        std::cout << std::numeric_limits<float>::min() << ", "
+                  << std::numeric_limits<char16_t>::max() << ", "
+                  << std::numeric_limits<double>::infinity() << ", "
+                  << std::numeric_limits<double>::quiet_NaN() << ", "
+                  << std::endl;
+
+        // Do note the differences between 'int' & 'float'
+        // For type 'double'
+        //  min()       ->  2.22507e-308	(still positive btw)
+        //  lowest()    ->  -1.79769e+308	(hugely neg number!)
+        std::cout << std::numeric_limits<int>::min() << ", "
+                  << std::numeric_limits<int>::lowest() << ", "
+                  << std::numeric_limits<double>::min() << ", "
+                  << std::numeric_limits<double>::lowest() << ", "
+                  << std::endl;
+    }
+
+    {
+        // Explicit type conversion
+
+        double d1{10.0};
+        double d2{5.5};
+        int d_resultOne{
+            static_cast<int>(d1) +
+            static_cast<int>(d2)}; // => 2 (10 / 5)
+
+        int d_resultTwo{
+            static_cast<int>(d1 + d2) // => 15 (15.5)
+        };
+
+        // Inexplicit type conversion
+
+        /*
+            The one having the wider range 
+                will assimilate all the other operands :D 
+
+            The ranks 
+                long double, double, float,
+                unsigned long long, long long,      
+                unsigned long,      long          
+                unsigned int,       int
+        */
+        int x{20u};     // -> unsigned internally
+        unsigned y{30}; // -10 => max(unsigned) - 10
+
+        std::cout << x - y << " Surprise!!\n"
+                  << std::endl;
+    }
+
+    {
+        // Formatting stream output
+
+        /*
+            Some of methods here were 
+                imported by `#include <iomanip>`
+
+            Most 'manipulator' remains in effect until you change it.
+                The only exception is the `std::setw()` (only the next). 
+        */
+        std::cout << std::setprecision(10)
+                  << std::setw(25)
+                  << std::fixed << 30.75 << ", "
+                  << std::scientific << 30.75 << ", "
+                  << std::defaultfloat << 30.75 << ", "
+                  << std::hex << 30 << ", "
+                  << std::showbase
+                  << std::hex << 30 << std::endl;
+    }
+
+    return 0;
+}
---
+++ Part_01/notes.md
@@ -0,0 +1,93 @@
+
+### X
+
+### Compiling commands
+- Here you are 
+
+    ```bash
+    # c++1z => C++ 17
+    clang++ -Wall -std=c++11 CODE_FILE BIN_NAME
+    clang++ -Wall -std=c++14 CODE_FILE BIN_NAME
+    clang++ -Wall -std=c++1z CODE_FILE BIN_NAME
+    ```
+
+### The *extensions*
+- ```.cpp```
+- ```.h```  ```.hpp```
+
+### *Statement*
+- I havn't *figure it out*, here's an example on the book
+
+    ```c++
+    int one { 1 };
+    int one {1};
+    int one{1};
+    int one{
+        1
+    };
+    ```
+
+### ```return``` statements
+- ```main()``` is the only func that not writing ```return``` is equivalent to ```return 0;```
+    - Any other func with return type ```int``` must **explicitly** ```return``` a value. 
+
+### *Namespace*
+- Things that are not defined in a namespace exist in the *global* namespace.
+
+### Variable names 
+- Names start with a ```_``` or ```__``` should **NOT** be used.
+- Cuz you *might* override the code gen_ed by *compiler* or ur *STL implementation*.
+
+### C++ *Templates* 
+- From my point of view, it is 
+    - a technique for (auto)generating the repeated code. 
+- Some info
+    1. There're *function templates*, and *class templates*.
+    2. The *Standard Library* uses templates **extensively**.
+- A glimpse 
+
+    ```c++
+    #include <iostream>
+    using namespace std;
+
+    template <class T>
+    T max(T a, T b)
+    {
+    	return a > b ? a : b ;
+    }
+    int main()
+    {
+        // This file cannot run correctly right now,
+        //  I'll fix it when I've got more knowledge of it.
+    	cout << max(1, 2)       << endl ;
+    	cout << max('k', 's')   << endl ;
+    	cout << max(10.1, 15.2) << endl ;
+    }
+    ``` 
+
+### Code Style 
+- Just an example 
+
+    ```c++
+    bool has_it_or_not(int x)
+    {
+        /*
+        No matter which one do you prefer, 
+            it's recommended to pick one style 
+            and then use it CONSISTENTLY throughout ur code.
+
+        In the real world, 
+            you either follow the company policies,
+            or on ur personal pref (based on nice linters!).
+        */
+    }
+    ```
+
+### Creating an *Executable*
+- It basically a 3-step process
+    1. Copy the entire contents of all ```#include``` to ur ```.cpp``` files 
+    2. Then ur compiler translates the code into *machine code*
+    3. The *linker* combines *all* the *object files* into *ONE* file containing the *complete executable program*.
+        - [all] your project typically contains more than one files 
+        - [object files] the file contains the machine code 
+        - [ONE] the executable binary file 
